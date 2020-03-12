77cc0a6c1c43929019b72575458ee7425d671280
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-12 19:03:06 +0100

    Create function for tables with valuebox

--- summary_tables.R
+++ summary_tables.R
@@ -1,19 +1,46 @@
+createSummaryTable <- function(data, column, title, icon, selectable = FALSE) {
+  valueBox_summary <- valueBox(
+    width    = 12,
+    format(sum(data[[column]]), big.mark = " "),
+    subtitle = title,
+    icon     = icon,
+    color    = "light-blue"
+  )
+
+  datatable_summary <- datatable(
+    as.data.frame(data),
+    rownames  = FALSE,
+    options   = list(
+      scrollY        = "calc(100vh - 300px)",
+      scrollCollapse = T,
+      dom            = 't',
+      paging         = FALSE
+    ),
+    selection = ifelse(selectable, "single", "none")
+  )
+
+  return(
+    renderUI({
+      box(
+        valueBox_summary,
+        renderDataTable(datatable_summary),
+        width  = 12,
+        height = "calc(100vh - 106px)"
+      )
+    })
+  )
+}
+
 summary_confirmed <- data_latest %>%
   group_by(`Country/Region`) %>%
   summarise(Confirmed = sum(confirmed)) %>%
   arrange(desc(Confirmed))
 
-output$table_confirmed <- renderDataTable(
-  datatable(
-    as.data.frame(summary_confirmed),
-    rownames = FALSE,
-    options  = list(
-      scrollY        = T,
-      scrollCollapse = T,
-      dom            = 't',
-      paging         = FALSE
-    )
-  )
+output$box_confirmed <- createSummaryTable(
+  summary_confirmed,
+  "Confirmed",
+  "Total Confirmed",
+  icon("file-medical")
 )
 
 summary_death <- data_latest %>%
@@ -21,17 +48,11 @@ summary_death <- data_latest %>%
   summarise(Deaths = sum(death)) %>%
   arrange(desc(Deaths))
 
-output$table_deaths <- renderDataTable(
-  datatable(
-    as.data.frame(summary_death),
-    rownames = FALSE,
-    options  = list(
-      scrollY        = T,
-      scrollCollapse = T,
-      dom            = 't',
-      paging         = FALSE
-    )
-  )
+output$box_deaths <- createSummaryTable(
+  summary_death,
+  "Deaths",
+  "Total Deaths",
+  icon("heartbeat")
 )
 
 summary_recovered <- data_latest %>%
@@ -39,15 +60,9 @@ summary_recovered <- data_latest %>%
   summarise(Recovered = sum(recovered)) %>%
   arrange(desc(Recovered))
 
-output$table_recovered <- renderDataTable(
-  datatable(
-    as.data.frame(summary_recovered),
-    rownames = FALSE,
-    options  = list(
-      scrollY        = T,
-      scrollCollapse = T,
-      dom            = 't',
-      paging         = FALSE
-    )
-  )
+output$box_recovered <- createSummaryTable(
+  summary_recovered,
+  "Recovered",
+  "Total Recovered",
+  icon("heartbeat")
 )
--- ui.R
+++ ui.R
@@ -10,63 +10,28 @@ ui <- dashboardPage(
     ),
     fluidRow(
       column(
+        uiOutput("box_confirmed"),
         width = 2,
-        valueBox(
-          width    = 12,
-          format(sum(data_latest$confirmed), big.mark = " "),
-          subtitle = "Total Confirmed",
-          icon     = icon("file-medical"),
-          color    = "light-blue"
-        ),
-        fluidRow(
-          column(
-            width = 12,
-            dataTableOutput("table_confirmed"),
-            style = "overflow-y: scroll; height: calc(100vh - 200px)"
-          )
-        )
+        style = "padding-left: 0px; padding-right: 0px"
       ),
       column(
-        width = 6,
         box(
           title = "Overview Map",
           width = 12,
           leafletOutput("overview_map")
-        )
+        ),
+        width = 6,
+        style = "padding-left: 0px; padding-right: 0px"
       ),
       column(
+        uiOutput("box_deaths"),
         width = 2,
-        valueBox(
-          width    = 12,
-          format(sum(data_latest$death), big.mark = " "),
-          subtitle = "Total Deaths",
-          icon     = icon("heartbeat"),
-          color    = "light-blue"
-        ),
-        fluidRow(
-          column(
-            width = 12,
-            dataTableOutput("table_deaths"),
-            style = "overflow-y: scroll; height: calc(100vh - 200px)"
-          )
-        )
+        style = "padding-left: 0px; padding-right: 0px"
       ),
       column(
+        uiOutput("box_recovered"),
         width = 2,
-        valueBox(
-          width    = 12,
-          format(sum(data_latest$recovered), big.mark = " "),
-          subtitle = "Total Recovered",
-          icon     = icon("heart"),
-          color    = "light-blue"
-        ),
-        fluidRow(
-          column(
-            width = 12,
-            dataTableOutput("table_recovered"),
-            style = "overflow-y: scroll; height: calc(100vh - 200px)"
-          )
-        )
+        style = "padding-left: 0px; padding-right: 0px"
       )
     )
   )
