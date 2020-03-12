701ee7fef76f03840f96abb048e2e1f3d38551a2
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-12 18:12:18 +0100

    Fixed bounds issue with map and made markers depending on zoom level

--- map.R
+++ map.R
@@ -1,25 +1,33 @@
 library("htmltools")
+
 data_latest$label <- paste0(
   '<b>', ifelse(is.na(data_latest$`Province/State`), data_latest$`Country/Region`, data_latest$`Province/State`), '</b><br>
   Confirmed: ', data_latest$confirmed, '<br>
   Deaths: ', data_latest$death, '<br>
-
   Recovered: ', data_latest$recovered
 )
 data_latest$label <- lapply(data_latest$label, HTML)
 
 map <- leaflet(data_latest) %>%
-  setMaxBounds(-90, -180, 90, 180) %>%
+  setMaxBounds(-180, -90, 180, 90) %>%
   setView(0, 0, zoom = 2) %>%
-  addTiles() %>%
-  addCircleMarkers(
-    lng          = ~Long,
-    lat          = ~Lat,
-    radius       = ~log(confirmed * 10),
-    stroke       = FALSE,
-    fillOpacity  = 0.5,
-    label        = ~label,
-    labelOptions = labelOptions(textsize = 15)
-  )
+  addTiles()
+
+observe({
+  zoomLevel <- input$overview_map_zoom
+  leafletProxy("overview_map", data = data_latest) %>%
+    clearMarkers() %>%
+    addCircleMarkers(
+      lng          = ~Long,
+      lat          = ~Lat,
+      radius       = ~log(confirmed^(zoomLevel / 2)),
+      stroke       = FALSE,
+      fillOpacity  = 0.5,
+      label        = ~label,
+      labelOptions = labelOptions(textsize = 15)
+    )
+})
 
 output$overview_map <- renderLeaflet(map)
+
+
