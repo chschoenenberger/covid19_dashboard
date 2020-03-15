bfff6c17a6c2537ba432c6991dea6a1ea912cbac
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-15 16:14:38 +0100

    Plot improvements (log axis possibility, improved legend etc.)

--- sections/content_plots/case_evolution.R
+++ sections/content_plots/case_evolution.R
@@ -6,7 +6,7 @@ output$case_evolution <- renderPlotly({
     ) %>%
     as.data.frame()
 
-  plot_ly(
+  p <- plot_ly(
     data,
     x     = ~date,
     y     = ~value,
@@ -18,25 +18,38 @@ output$case_evolution <- renderPlotly({
       yaxis = list(title = "# cases"),
       xaxis = list(title = "Date")
     )
+
+
+  if (input$checkbox_logCaseEvolution) {
+    p <- layout(p, yaxis = list(type = "log"))
+  }
+
+  return(p)
 })
 
+top5_countries             <- data_evolution %>%
+  group_by(`Country/Region`) %>%
+  summarise(value = sum(value)) %>%
+  arrange(desc(value)) %>%
+  top_n(5) %>%
+  select(`Country/Region`) %>%
+  pull()
 output$selectize_countries <- renderUI({
-  column(
-    selectizeInput(
-      "caseEvolution_country",
-      label    = "Select countries",
-      choices  = unique(data_evolution$`Country/Region`),
-      selected = c("China", "Italy", "Iran"),
-      multiple = TRUE
-    ),
-    width = 3
+  selectizeInput(
+    "caseEvolution_country",
+    label    = "Select countries",
+    choices  = unique(data_evolution$`Country/Region`),
+    selected = top5_countries,
+    multiple = TRUE
   )
 })
 
 getDataByCountry <- function(countries) {
   data_confirmed <- data_evolution %>%
     select(`Country/Region`, date, var, value) %>%
-    filter(`Country/Region` %in% countries & var == "confirmed") %>%
+    filter(`Country/Region` %in% countries &
+      var == "confirmed" &
+      value > 0) %>%
     group_by(`Country/Region`, date) %>%
     summarise("Confirmed" = sum(value)) %>%
     arrange(date) %>%
@@ -44,7 +57,9 @@ getDataByCountry <- function(countries) {
 
   data_recovered <- data_evolution %>%
     select(`Country/Region`, date, var, value) %>%
-    filter(`Country/Region` %in% countries & var == "recovered") %>%
+    filter(`Country/Region` %in% countries &
+      var == "recovered" &
+      value > 0) %>%
     group_by(`Country/Region`, date) %>%
     summarise("Recovered" = sum(value)) %>%
     arrange(date) %>%
@@ -52,7 +67,9 @@ getDataByCountry <- function(countries) {
 
   data_death <- data_evolution %>%
     select(`Country/Region`, date, var, value) %>%
-    filter(`Country/Region` %in% countries & var == "death") %>%
+    filter(`Country/Region` %in% countries &
+      var == "death" &
+      value > 0) %>%
     group_by(`Country/Region`, date) %>%
     summarise("Death" = sum(value)) %>%
     arrange(date) %>%
@@ -67,6 +84,7 @@ getDataByCountry <- function(countries) {
 
 output$case_evolution_byCountry <- renderPlotly({
   data <- getDataByCountry(input$caseEvolution_country)
+
   req(nrow(data$confirmed) > 0)
   p <- plot_ly(data = data$confirmed, x = ~date, y = ~Confirmed, color = ~`Country/Region`, type = 'scatter', mode = 'lines',
     legendgroup     = ~`Country/Region`) %>%
@@ -74,10 +92,20 @@ output$case_evolution_byCountry <- renderPlotly({
       legendgroup  = ~`Country/Region`, showlegend = FALSE) %>%
     add_trace(data = data$death, x = ~date, y = ~Death, color = ~`Country/Region`, line = list(dash = 'dot'),
       legendgroup  = ~`Country/Region`, showlegend = FALSE) %>%
+    add_trace(data = data$confirmed[which(data$confirmed$`Country/Region` == input$caseEvolution_country[1]),],
+      x            = ~date, y = -100, line = list(color = 'rgb(0, 0, 0)'), legendgroup = 'helper', name = "Confirmed") %>%
+    add_trace(data = data$confirmed[which(data$confirmed$`Country/Region` == input$caseEvolution_country[1]),],
+      x            = ~date, y = -100, line = list(color = 'rgb(0, 0, 0)', dash = 'dash'), legendgroup = 'helper', name = "Recovered") %>%
+    add_trace(data = data$confirmed[which(data$confirmed$`Country/Region` == input$caseEvolution_country[1]),],
+      x            = ~date, y = -100, line = list(color = 'rgb(0, 0, 0)', dash = 'dot'), legendgroup = 'helper', name = "Death") %>%
     layout(
-      yaxis = list(title = "# cases"),
+      yaxis = list(title = "# cases", range = c(0, max(data$confirmed$Confirmed))),
       xaxis = list(title = "Date")
     )
+
+  if (input$checkbox_logCaseEvolutionCountry) {
+    p <- layout(p, yaxis = list(type = "log", range = log(c(2, max(data$confirmed$Confirmed)))))
+  }
 
   return(p)
 })
@@ -87,12 +115,30 @@ output$box_caseEvolution <- renderUI({
     box(
       title = "Evolution of Cases since Outbreak",
       plotlyOutput("case_evolution"),
+      column(
+        checkboxInput("checkbox_logCaseEvolution", label = "Logaritmic Y-Axis", value = FALSE),
+        width = 3,
+        style = "float: right; padding: 10px; margin-right: 50px"
+      ),
       width = 6
     ),
     box(
       title = "Cases per Country",
       plotlyOutput("case_evolution_byCountry"),
-      uiOutput("selectize_countries"),
-      width = 6
+      fluidRow(
+        column(
+          uiOutput("selectize_countries"),
+          width = 3,
+        ),
+        column(
+          ,
+          width = 2
+        ),
+        column(
+          checkboxInput("checkbox_logCaseEvolutionCountry", label = "Logaritmic Y-Axis", value = FALSE),
+          width = 3,
+          style = "float: right; padding: 10px; margin-right: 50px"
+        )
+      )
     ))
 })
