6b2573f4885c30011f37c53581cdd8d39835008d
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-22 16:18:22 +0100

    Zoom to Country / Province on click on summary_table.R

--- sections/content_overview/summary_table.R
+++ sections/content_overview/summary_table.R
@@ -15,9 +15,9 @@ output$summaryTables <- renderUI({
   )
 })
 
-output$summaryDT_country <- renderDataTable(getSummaryDT(data_atDate(current_date), "Country/Region"))
+output$summaryDT_country <- renderDataTable(getSummaryDT(data_atDate(current_date), "Country/Region", selectable = TRUE))
 proxy_summaryDT_country  <- dataTableProxy("summaryDT_country")
-output$summaryDT_state   <- renderDataTable(getSummaryDT(data_atDate(current_date), "Province/State"))
+output$summaryDT_state   <- renderDataTable(getSummaryDT(data_atDate(current_date), "Province/State", selectable = TRUE))
 proxy_summaryDT_state    <- dataTableProxy("summaryDT_state")
 
 observeEvent(input$timeSlider, {
@@ -25,6 +25,35 @@ observeEvent(input$timeSlider, {
   replaceData(proxy_summaryDT_country, summariseData(data, "Country/Region"), rownames = FALSE)
   replaceData(proxy_summaryDT_state, summariseData(data, "Province/State"), rownames = FALSE)
 }, ignoreInit = TRUE, ignoreNULL = TRUE)
+
+observeEvent(input$summaryDT_country_row_last_clicked, {
+  selectedRow     <- input$summaryDT_country_row_last_clicked
+  selectedCountry <- summariseData(data_atDate(input$timeSlider), "Country/Region")[selectedRow, "Country/Region"]
+  location        <- data_evolution %>%
+    distinct(`Country/Region`, Lat, Long) %>%
+    filter(`Country/Region` == selectedCountry) %>%
+    summarise(
+      Lat  = mean(Lat),
+      Long = mean(Long)
+    )
+  leafletProxy("overview_map") %>%
+    setView(lng = location$Long, lat = location$Lat, zoom = 4)
+})
+
+observeEvent(input$summaryDT_state_row_last_clicked, {
+  selectedRow     <- input$summaryDT_state_row_last_clicked
+  selectedCountry <- summariseData(data_atDate(input$timeSlider), "Province/State")[selectedRow, "Province/State"]
+  browser()
+  location <- data_evolution %>%
+    distinct(`Province/State`, Lat, Long) %>%
+    filter(`Province/State` == selectedCountry) %>%
+    summarise(
+      Lat  = mean(Lat),
+      Long = mean(Long)
+    )
+  leafletProxy("overview_map") %>%
+    setView(lng = location$Long, lat = location$Lat, zoom = 4)
+})
 
 summariseData <- function(df, groupBy) {
   df %>%
