aac344635d7ac3bc9801fc1d72e53596dd66a008
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-22 14:11:41 +0100

    Use active cases at latest date for top5 countries

--- global.R
+++ global.R
@@ -109,7 +109,7 @@ rm(population, countryNamesPop, countryNamesDat, noDataCountries)
 data_latest <- data_atDate(max(data_evolution$date))
 
 top5_countries <- data_evolution %>%
-  filter(var == "active") %>%
+  filter(var == "active", date == current_date) %>%
   group_by(`Country/Region`) %>%
   summarise(value = sum(value)) %>%
   arrange(desc(value)) %>%
