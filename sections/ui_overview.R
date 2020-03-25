6e4b685f4f5dea256e9bb6de9524db06fe96b41b
Jacob McGowan <jacobmcgowan78@gmail.com>
2020-03-24 20:43:00 -0400

    Moved slider above table on mobile.

--- sections/ui_overview.R
+++ sections/ui_overview.R
@@ -2,28 +2,33 @@ body_overview <- dashboardBody(
   tags$head(
     tags$style(type = "text/css", "#overview_map {height: 48vh !important;}"),
     tags$style(type = 'text/css', ".slider-animate-button { font-size: 20pt !important; }"),
-    tags$style(type = 'text/css', ".slider-animate-container { text-align: left !important; }")
-
+    tags$style(type = 'text/css', ".slider-animate-container { text-align: left !important; }"),
+    tags$style(type = "text/css", "@media (max-width: 991px) { .details { display: flex; flex-direction: column; } }"),
+    tags$style(type = "text/css", "@media (max-width: 991px) { .details .map { order: 1; width: 100%; } }"),
+    tags$style(type = "text/css", "@media (max-width: 991px) { .details .summary { order: 3; width: 100%; } }"),
+    tags$style(type = "text/css", "@media (max-width: 991px) { .details .slider { order: 2; width: 100%; } }")
   ),
   fluidRow(
     fluidRow(
       uiOutput("box_keyFigures")
     ),
     fluidRow(
+      class = "details",
       column(
         box(
           width = 12,
           leafletOutput("overview_map")
         ),
+        class = "map",
         width = 8,
         style = 'padding:0px;'
       ),
       column(
         uiOutput("summaryTables"),
+        class = "summary",
         width = 4,
         style = 'padding:0px;'
-      )),
-    fluidRow(
+      ),
       column(
         sliderInput(
           "timeSlider",
@@ -35,6 +40,7 @@ body_overview <- dashboardBody(
           timeFormat = "%d.%m.%Y",
           animate    = animationOptions(loop = TRUE)
         ),
+        class = "slider",
         width = 12,
         style = 'padding-left:15px; padding-right:15px;'
       )
