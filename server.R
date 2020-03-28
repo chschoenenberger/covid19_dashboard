1b22833b6340b6f44db3e083f95cdb773371e47c
Christoph Schönenberger <ch.schoenenberger@outlook.com>
2020-03-28 15:25:50 +0100

    Only show duration for 30s

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ server <- function(input, output) {
 
   showNotification("Attention: Johns Hopkins University does not provide recovered cases anymore. Therefore,
   recovered cases are an estimation only. Please check the about section for more information.",
-    duration = NULL, type = "error")
+    duration = 30, type = "error")
 
   # Trigger once an hour
   dataLoadingTrigger <- reactiveTimer(3600000)
